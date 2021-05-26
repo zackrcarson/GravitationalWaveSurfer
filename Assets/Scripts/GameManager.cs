589549780325405384968df5e38036ea5db0f35c
zackrcarson <69831285+zackrcarson@users.noreply.github.com>
2021-05-26 17:55:40 -0600

    Added score board showing number of protons/neutrons/electrons, and also something showing the element name, atomic number, mass number, and ionic number
    Added score board showing number of protons/neutrons/electrons, and also something showing the element name, atomic number, mass number, and ionic number

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,33 @@
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
     // State Variables
+    [Header("Initial Numbers")]
     [SerializeField] int numProtons = 1;
     [SerializeField] int numNeutrons = 1;
     [SerializeField] int numElectrons = 1;
+
+    [Header("Text Boxes")]
+    [SerializeField] Text protonsText = null;
+    [SerializeField] Text neutronsText = null;
+    [SerializeField] Text electronsText = null;
+
+    [SerializeField] Text elementText = null;
+    [SerializeField] Text massNumberText = null;
+    [SerializeField] Text AtomicNumberText = null;
+    [SerializeField] Text IonicNumberText = null;
+
+    [Header("Data")]
+    [SerializeField] string elementsData = "Assets/Data/elements.csv";
+    
+    // Cached References
+    Dictionary<int, string> elementsDict = null;
 
     // Constants
     const string PROTON_NAME = "Proton";
@@ -19,6 +39,65 @@ public class GameManager : MonoBehaviour
         instance = this;
 
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        CollectElements();
+
+        ShowScore();
+    }
+
+    private void CollectElements()
+    {
+        elementsDict = new Dictionary<int, string>();
+
+        StreamReader strReader = new StreamReader(elementsData);
+        bool endOfFile = false;
+
+        while (!endOfFile)
+        {
+            string dataString = strReader.ReadLine();
+
+            if (dataString == null)
+            {
+                endOfFile = true;
+                break;
+            }
+
+            string[] dataValues = dataString.Split(',');
+
+            elementsDict.Add(int.Parse(dataValues[0]), dataValues[1]);
+        }
+    }
+
+    private void ShowScore()
+    {
+        protonsText.text = numProtons.ToString();
+        neutronsText.text = numNeutrons.ToString();
+        electronsText.text = numElectrons.ToString();
+
+        elementText.text = elementsDict[numProtons];
+        massNumberText.text = (numProtons + numNeutrons).ToString();
+        AtomicNumberText.text = numProtons.ToString();
+
+        if (numElectrons == numProtons)
+        {
+            IonicNumberText.gameObject.SetActive(false);
+        }
+        else
+        {
+            IonicNumberText.gameObject.SetActive(true);
+
+            if (numElectrons > numProtons)
+            {
+                IonicNumberText.text = "+" + (numElectrons - numProtons).ToString();
+            }
+            else if (numElectrons < numProtons)
+            {
+                IonicNumberText.text = "-" + (numProtons - numElectrons).ToString();
+            }
+        }
     }
 
     public void AddParticle(string type)
@@ -39,5 +118,7 @@ public class GameManager : MonoBehaviour
         {
             Debug.LogError("Unknown particle (" + type + ") not added!");
         }
+
+        ShowScore();
     }
 }
