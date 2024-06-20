4bb2bf0364f5b20f3980de03e77906da1518be1c
Mush <greenfishfungus2@gmail.com>
2024-06-20 14:59:44 -0400

    remodel player

---
+++ Assets/GravitationalWaveSurfer/Source/GWS/Player/Runtime/DrawAntenna.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawAntenna : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] joints;
+    private LineRenderer line;
+
+    private void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < joints.Length; i++)
+        {
+            line.SetPosition(i, joints[i].transform.position);
+        }
+    }
+}
